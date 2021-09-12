87b158597d6b4e5cd048068859aeb6ef0cf340e9
mohak95 <mohakkumar1995@gmail.com>
2021-09-12 21:03:55 +0530

    first commit

--- ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
+++ ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
@@ -1,10 +1,9 @@
 package com.shopme.admin.user;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends CrudRepository<Role, Integer>{
+public interface RoleRepository extends Crud{
 	
 
 }
